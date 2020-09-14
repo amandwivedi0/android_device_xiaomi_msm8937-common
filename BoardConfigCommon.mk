0c16746a36d874b73b4fa796f007c54d3e92b5f6
amandwivedi0 <anshuman.p01999@gmail.com>
2020-09-14 17:26:28 +0000

    [TMP] msm8937-common:disable sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -207,7 +207,7 @@ TARGET_RECOVERY_FSTAB 		 := $(VENDOR_PATH)/rootdir/fstab.qcom
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true

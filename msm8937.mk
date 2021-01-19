9eb5d30f01bbfd301d9367a2ecafe59c943b33b5
intervigilium <intervigil@gmail.com>
2021-01-19 03:48:00 +0000

    msm8937-common: Add basic USB HAL that reports no status change

--- msm8937.mk
+++ msm8937.mk
@@ -399,7 +399,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.santoni
 
 # Vibrator
 PRODUCT_PACKAGES += \

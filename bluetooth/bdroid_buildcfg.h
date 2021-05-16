b4680d9afc78bde04174e8b93d90ecca29332287
amandwivedi0 <anshuman.p01999@gmail.com>
2021-05-16 12:34:37 +0000

    msm8937-common: bluetooth: update bdroid_buildcfg.h *from https://github.com/Jabiyeff-Project/device-santoni/blob/11.0/configs/bluetooth/bdroid_buildcfg.h

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,6 +1,5 @@
-/**
- * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2016 The CyanogenMod Project
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,27 +16,22 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
+#define BTM_DEF_LOCAL_NAME   "Xiaomi Redmi 4X"
 
-#include <string.h>
-
-#define MAX_ACL_CONNECTIONS    16
+// Disables read remote device feature
+#define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
 
-// Wide-band speech support
-#define BTIF_HF_WBS_PREFERRED TRUE
-
 // skips conn update at conn completion
-#define BT_CLEAN_TURN_ON_DISABLED 1
+#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
+
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
 
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
 #define AVDT_NUM_SEPS 12
 
 /* Enable HFP WBS feature */
 #define BTIF_HF_CLIENT_WBS_INCLUDED TRUE
-
-/* Disable enhanced sco commands */
-#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
-#define BTM_SCO_ENHANCED_SYNC_DISABLED TRUE
-
-#endif /* _BDROID_BUILDCFG_H */
+#endif
